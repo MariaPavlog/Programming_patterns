36d7934cce3673882369a5c52b98ddfaf00b23b2
MariaPavlog <pavlogradskaam@gmail.com>
2024-11-25 19:26:43 +0300

    LB2 P2 exp2 методы класса DataTable

--- LB1/src/main/kotlin/DataTable.kt
+++ LB1/src/main/kotlin/DataTable.kt
@@ -1,4 +1,7 @@
 package org.example
 
-class DataTable(private val array: Array<Array<Any>>) {
+class DataTable(private val array: Array<Array<Any>>){
+fun get(row: Int, am: Int) = array[row][am]
+fun getRowAmount() = array.size
+fun getColAmount() = if (array.isNotEmpty()) array[0].size else 0
 }
