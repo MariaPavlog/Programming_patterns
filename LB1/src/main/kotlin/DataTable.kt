dc3e07d33604d1ae7926043e4b5d5d1a15fe8df9
MariaPavlog <pavlogradskaam@gmail.com>
2024-11-25 19:43:36 +0300

    LB2 P2 exp5 создание класса DataListStudentShort и изменения в других классах для его работы

--- LB1/src/main/kotlin/DataTable.kt
+++ LB1/src/main/kotlin/DataTable.kt
@@ -1,7 +1,7 @@
 package org.example
 
-class DataTable(private val array: Array<Array<Any>>){
-fun get(row: Int, am: Int) = array[row][am]
+class DataTable(private val array: List<List<Any?>>){
+    operator fun get(row: Int, col: Int) = array[row][col]
 fun getRowAmount() = array.size
 fun getColAmount() = if (array.isNotEmpty()) array[0].size else 0
 }
